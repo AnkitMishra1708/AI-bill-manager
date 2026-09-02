56221adc99f1576f5d96f71aad207fd60592c0c9
Ankit <mishraankit1708ak@gmail.com>
2026-09-02 19:00:42 +0530

    checking bugs

--- Server/src/routes/payment.route.js
+++ Server/src/routes/payment.route.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { createOrder, validateWebhook } from "../controllers/payment.controller.js";
+import {
+  createOrder,
+  validateWebhook,
+} from "../controllers/payment.controller.js";
 
 const router = express.Router();
 
 router.route("/order").post(verifyJwt, createOrder);
-router.route("/webhook").post(validateWebhook);
+router.route("/webhook").post(() => {
+  console.log("Webhook Route hit");
+}, validateWebhook);
 
 export default router;
